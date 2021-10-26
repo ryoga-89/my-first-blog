5fbbdc556fc104fcf868fc4db7be16f583352d15
釼持凌我 <ryogatti-12.8.9@i.softbank.jp>
2021-10-26 23:38:06 +0900

    Added view and template for detailed blog post as well as CSS for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from typing_extensions import _AnnotatedAlias
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,6 @@ SECRET_KEY = 'niv#x74^ah65)5t)qsl6c1ed%ji96#&e@o^yo@^p%+&_t1m0+a'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
-
 
 # Application definition
 
